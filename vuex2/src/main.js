862840067e013bb7a76eaf5f684c1507618c3177
Rodrigo Angelo Valentini <rodrigoangelovalentini@hotmail.com>
2016-11-25 11:57:36 +0000

    vuex

---
+++ vuex2/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import './filters';
+import AppComponent from './components/app.component';
+import store from './store';
+
+require('style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css');
+require('file-loader!./assets/');
+require('bootstrap');
+
+
+let meuVue = new Vue({
+    el: '#app',
+    components: {
+        'app': AppComponent
+    },
+    store
+});
